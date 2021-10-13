975ea409f4c5d892654a5afcbdfa3fe688ff51ef
Bálint Aradi <aradi@uni-bremen.de>
2021-10-13 18:27:57 +0200

    Add error propagation path recording

--- src/errorfx.f90
+++ src/errorfx.f90
@@ -1,9 +1,19 @@
 !> Implements an exception like error handling mechanism in Fortran.
 module errorfx
+  use iso_fortran_env, only : stderr => error_unit
   implicit none
 
   private
   public :: fatal_error, create, init
+
+
+  type :: linked_location
+    character(:), allocatable :: file
+    integer :: line
+    type(linked_location), allocatable :: previous
+  contains
+    procedure :: write_formatted => linked_location_write_formatted
+  end type linked_location
 
 
   !> Base class for fatal errors (stops code if an unhandled error goes out of scope)
@@ -19,6 +29,9 @@ module errorfx
     !> Error message (Unallocated, if not set explicitely)
     character(:), allocatable :: message
 
+    !> Contains (if filled up) the path along which the error was propagated upwards
+    type(linked_location), allocatable :: propagation_path
+
     !> Whether error is active
     logical, private :: active = .false.
 
@@ -29,6 +42,9 @@ module errorfx
 
     !> Queries whether the error is still active
     procedure :: is_active => fatal_error_is_active
+
+    !> Adds information about the error propagation path
+    procedure :: add_propagation_info => fatal_error_add_propagation_info
 
     !> Finalizer for the error. (You may consider to include it in debug-mode only)
     final :: fatal_error_final
@@ -91,7 +107,7 @@ contains
 
 
   !> Finalizer for a critical error. Stops the code if the error is still active.
-  pure subroutine fatal_error_final(this)
+  subroutine fatal_error_final(this)
 
     !> Instance
     type(fatal_error), intent(inout) :: this
@@ -99,8 +115,14 @@ contains
     character(:), allocatable :: errormsg
 
     if (this%active) then
-      errormsg = "Stopping due to unhandled critical error: " // this%message
-      error stop errormsg
+      write(stderr, "(a)") "Stopping due to unhandled critical error"
+      if (allocated(this%message)) write(stderr, "(2a)") "Error message: ", this%message
+      write(stderr, "(a, i0)") "Error code: ", this%code
+      if (allocated(this%propagation_path)) then
+        write(stderr, "(a)") "Error propagation path:"
+        call this%propagation_path%write_formatted(stderr)
+      end if
+      error stop
     end if
 
   end subroutine fatal_error_final
@@ -108,6 +130,8 @@ contains
 
   !> Deactivates the error (error would not stop the code if going out of scope)
   pure subroutine fatal_error_deactivate(this)
+
+    !> Instance
     class(fatal_error), intent(inout) :: this
 
     this%active = .false.
@@ -117,11 +141,56 @@ contains
 
   !> Queries, whether the error is still active or it had been already deactivated
   pure function fatal_error_is_active(this) result(is_active)
+
+    !> Instance.
     class(fatal_error), intent(in) :: this
+
+    !> Whether error is still active
     logical :: is_active
 
     is_active = this%active
 
   end function fatal_error_is_active
+
+
+  !> Adds propagation information to the error
+  pure subroutine fatal_error_add_propagation_info(this, file, line)
+
+    !> Instance
+    class(fatal_error), intent(inout) :: this
+
+    !> File where error occured
+    character(*), intent(in) :: file
+
+    !> Line where error occured
+    integer, intent(in) :: line
+
+    type(linked_location), allocatable :: curloc
+
+    allocate(curloc)
+    curloc%file = file
+    curloc%line = line
+    call move_alloc(this%propagation_path, curloc%previous)
+    call move_alloc(curloc, this%propagation_path)
+
+  end subroutine fatal_error_add_propagation_info
+
+
+  !> Writes the chain of locations to a unit
+  subroutine linked_location_write_formatted(this, unit)
+
+    !> Instance
+    class(linked_location), intent(in) :: this
+
+    !> Unit to write the location chain
+    integer, intent(in) :: unit
+
+    if (allocated(this%previous)) then
+      call this%previous%write_formatted(unit)
+    end if
+    write(unit, "(a, ':', i0)") this%file, this%line
+
+  end subroutine linked_location_write_formatted
+
 
 end module errorfx
